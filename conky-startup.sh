0298908872cb4d949e40a63d1ac0493f6a7d308d
Tonipt <1972vilas@sapo.pt>
2016-11-30 03:01:03 +0000

    Update conky-startup.sh

--- conky-startup.sh
+++ conky-startup.sh
@@ -3,4 +3,4 @@ killall conky
 cd ~/.conky/meu-conky/
 conky -c ~/.conky/meu-conky/infobar &
 # conky -c ~/.conky/meu-conky/cal13meses &
-# conky -c ~/.conky/meu-conky/yahoometeo &
+# conky -c ~/.conky/meu-conky/yahoometeo
